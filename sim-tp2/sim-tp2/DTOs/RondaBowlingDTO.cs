e391a9b8909f417156fca542a087054a7ed60a24
Benjamin <benjagarate1@gmail.com>
2024-04-27 17:23:27 -0300

    Add: DTO Bowling

---
+++ sim-tp2/sim-tp2/DTOs/RondaBowlingDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sim_tp2.DTOs
+{
+    public class RondaBowlingDto
+    {
+        public int NumeroRonda { get; set; }
+
+        public double RandomPrimerTiro { get; set; }
+
+        public int PinosTiradosPrimerTiro { get; set; }
+
+        public double? RandomSegundoTiro { get; set; }
+
+        public int? PinosTiradosSegundoTiro { get; set; }
+
+        public int PinosTotales { get; set; }
+
+        public int PuntajeObtenido { get; set; }
+
+        public int PuntajeAcumuladoJuego { get; set; }
+
+        public int ContadorExitos { get; set; }
+
+        public double ProbabilidadExito { get; set; }
+    }
+}
