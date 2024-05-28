2c9ea20157b3ae81e52c713eb8e1a54ef113679e
GuadalupeAcosta1 <guadalupeacostacode@gmail.com>
2024-05-28 11:43:58 -0300

    Agrego front

--- sim-tp2/sim-tp2/Views/MenuPrincipal.Designer.cs
+++ sim-tp2/sim-tp2/Views/MenuPrincipal.Designer.cs
@@ -32,31 +32,35 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button_colas = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(348, 42);
+            this.label1.Location = new System.Drawing.Point(261, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Simulación";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(326, 82);
+            this.label2.Location = new System.Drawing.Point(244, 67);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(121, 16);
+            this.label2.Size = new System.Drawing.Size(95, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Trabajos Prácticos";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(251, 270);
+            this.button1.Location = new System.Drawing.Point(188, 219);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(259, 44);
+            this.button1.Size = new System.Drawing.Size(194, 36);
             this.button1.TabIndex = 2;
             this.button1.Text = "Distribuciones";
             this.button1.UseVisualStyleBackColor = true;
@@ -64,23 +68,37 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(251, 329);
+            this.button2.Location = new System.Drawing.Point(188, 267);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(259, 44);
+            this.button2.Size = new System.Drawing.Size(194, 36);
             this.button2.TabIndex = 3;
             this.button2.Text = "Método de Montecarlo";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button_colas
+            // 
+            this.button_colas.Location = new System.Drawing.Point(188, 319);
+            this.button_colas.Margin = new System.Windows.Forms.Padding(2);
+            this.button_colas.Name = "button_colas";
+            this.button_colas.Size = new System.Drawing.Size(194, 36);
+            this.button_colas.TabIndex = 4;
+            this.button_colas.Text = "Sistema de colas";
+            this.button_colas.UseVisualStyleBackColor = true;
+            this.button_colas.Click += new System.EventHandler(this.button_colas_Click);
+            // 
             // MenuPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button_colas);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MenuPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MenuPrincipal";
@@ -95,5 +113,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button_colas;
     }
 }
